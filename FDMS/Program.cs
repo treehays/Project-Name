c1527e5f4be2eae42eeebf36bcdff1719dbe790c
Treehays90 <33053528+Treehays90@users.noreply.github.com>
2022-10-11 18:31:14 +0100

    add clom rto my program

--- FDMS/Program.cs
+++ FDMS/Program.cs
@@ -1,6 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// See https
+//aka.ms/new-console-tesmplate for more information
 // Console.WriteLine("Hello, World!");
 
 
-
+iushkdfksdbhkskdshuak
 //  this is first time
